4bf65781f4b0ef173b6a7cd4e4cdec26d1d48881
Cleber Lima <cleberlima96@gmail.com>
2019-04-03 17:00:02 -0300

    Ajustes de entrada na main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         Criptografia c = new Criptografia();
-        char[] crip = c.criptografar("Paulo Emir".toCharArray(), "UPECARUARU".toCharArray());
+        String chave = "UPECARUARU";
+        String frase = "Paulo Emir";
+        
+        char[] crip = c.criptografar(frase.toCharArray(), chave.toCharArray());
 
         System.out.println("CRIPTOGRAFADO: " + String.copyValueOf(crip));
         
-        char[] decrip = c.descriptografar(crip, "UPECARUARU".toCharArray());
+        char[] decrip = c.descriptografar(crip, chave.toCharArray());
          
         System.out.println("DESCRIPTOGRAFADO: " + String.copyValueOf(decrip));
 
