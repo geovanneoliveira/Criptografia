9f1d5e789816d07dced7571299bee21779c4a085
Cleber Lima <cleberlima96@gmail.com>
2019-04-04 16:06:08 -0300

    Revert "Ajustes de entrada na main"
    This reverts commit 4bf65781f4b0ef173b6a7cd4e4cdec26d1d48881.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         Criptografia c = new Criptografia();
-        String chave = "UPECARUARU";
-        String frase = "Paulo Emir";
-        
-        char[] crip = c.criptografar(frase.toCharArray(), chave.toCharArray());
+        char[] crip = c.criptografar("Paulo Emir".toCharArray(), "UPECARUARU".toCharArray());
 
         System.out.println("CRIPTOGRAFADO: " + String.copyValueOf(crip));
         
-        char[] decrip = c.descriptografar(crip, chave.toCharArray());
+        char[] decrip = c.descriptografar(crip, "UPECARUARU".toCharArray());
          
         System.out.println("DESCRIPTOGRAFADO: " + String.copyValueOf(decrip));
 
